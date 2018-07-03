d4bd57aace5c6253dbe82553b33e51304c7fafa7
fengerhu1 <2748250768@qq.com>
2018-07-03 14:59:32 +0800

    add matlab

---
+++ 人机交互/Api/src/Api.java
@@ -0,0 +1,42 @@
+public class Api {
+
+    public class job
+    {
+        private int startTime;
+        private int stopTime;
+        private int perPower;
+        public void setStartTime(int startTime)
+        {
+            this.startTime = startTime;
+        }
+        public int getStartTime()
+        {
+            return this.startTime;
+        }
+
+        public void setStopTime(int stopTime)
+        {
+            this.stopTime = stopTime;
+        }
+        public int getStopTime()
+        {
+            return this.stopTime;
+        }
+        public void setPerPower(int perPower)
+        {
+            this.perPower = perPower;
+        }
+        public int getPerPower()
+        {
+            return this.perPower;
+        }
+    }
+
+    public class forecastSolarPower
+    {
+
+    }
+
+
+
+}
