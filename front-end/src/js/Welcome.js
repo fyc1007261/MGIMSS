c69641d4cd611fda2fcec39d0f6095bc52db5dbb
Breezeeee <S-J-T-U-j-f-x@sjtu.edu.cn>
2018-07-04 10:41:04 +0800

    welcome page

--- front-end/src/js/Welcome.js
+++ front-end/src/js/Welcome.js
@@ -13,17 +13,23 @@ class Welcome extends Component {
     render() {
         console.log(this.state.redirect);
         if(this.state.redirect) {
-            return(
+            return (
                 <Redirect push to="/login"/>
             );
         }
         return(
-            <div id="Welcome" className="alt">
-                <div className="inner">
-                    <h1>MGIMSS</h1>
-                    <p>xxxxxx xxxxxx xxxxxx xxxxx xxxxxxx xxxxxxxxx xxxx</p>
+            <div>
+                <div className="fade">
                 </div>
-                <button onClick={this.handleLoginClick}>Login</button>
+                <header id="header" className="alt">
+                    <div className="inner">
+                        <h1>MGIMSS</h1>
+                        <p>xxxxxxxx xxxxxxxxxx xxxx xxxxx x xxxxxx xxxxxx xxxx</p>
+                    </div>
+                </header>
+                <div className="WelcomeButton">
+                    <button onClick={this.handleLoginClick}>Login</button>
+                </div>
             </div>
         );
     }
