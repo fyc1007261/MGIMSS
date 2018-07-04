2931655275d91a8730b920b241e188e348aa2f6e
fengerhu1 <2748250768@qq.com>
2018-07-04 16:14:12 +0800

    addMatlab

--- 人机交互/matlab/src/getForecastData.java
+++ 人机交互/matlab/src/getForecastData.java
@@ -8,11 +8,14 @@ import demo.demo;
 import train.train;
 //需要加入javabuild包 在V91的builertool中的javabuild中
 
-public class test {
+public class getForecastData {
     public static void main(String[] args) throws Exception{
+        double[] x={0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1};
+        predicted(x);
+    }
+    public static double[] predicted(double[] x) throws Exception{
         //System.load("D://大二暑假项目 - 副本//matlab//mclmcrrt9_1.dll");
         Object[] result = null; // 用于保存计算结果
-        double x[]={0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1};
         MWNumericArray a = null;
         a = new MWNumericArray(x, MWClassID.DOUBLE);
         train thePlot = null; // plotter类的实例（在MatLab编译时，新建的类）
@@ -22,9 +25,9 @@ public class test {
 
         MWCellArray output=(MWCellArray)result[0];
 
-//cellArray.numberOfElements() 获取元胞数组中元素总个数
-//MWCellArray 相当于把表格数据纵向一维展开， 每一列的数据是相连的 而封装java对象要抽出一行的数据
-//元素总个数除以列数（表具有的字段数）得到行数
+        //cellArray.numberOfElements() 获取元胞数组中元素总个数
+        //MWCellArray 相当于把表格数据纵向一维展开， 每一列的数据是相连的 而封装java对象要抽出一行的数据
+        //元素总个数除以列数（表具有的字段数）得到行数
         int rowNum= output.numberOfElements() ;
         //ArrayList forecastResult = new ArrayList();
         double[] forecastResult;
@@ -34,11 +37,10 @@ public class test {
             forecastResult[i-1] = ans;
             System.out.println(ans);
         }
-
         System.out.println("succeed");
         System.out.println(forecastResult[0]);
         MWArray.disposeArray(a);
         MWArray.disposeArray(result);
+        return forecastResult;
     }
-
 }
