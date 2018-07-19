8e658877c21b813ff172de2ad10d3106e370b9c1
fyc1007261 <fyc1007261@live.com>
2018-07-19 10:39:54 +0800

    integrate chart with app details

---
+++ back-end/src/test/java/com/mgimss/mgimss/controller/ShowAppInfoImplTests.java
@@ -0,0 +1,19 @@
+package com.mgimss.mgimss.controller;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShowAppInfoImplTests {
+
+
+    @Test
+    public void contextLoads(){
+
+    }
+
+}
