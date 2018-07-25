dc9fe6f9fd546c0414a22e1c423b5d4d9d7f05ec
Breezeeee <S-J-T-U-j-f-x@sjtu.edu.cn>
2018-07-25 16:13:46 +0800

    upload to cloudinary

--- fron-end/src/_nav.js
+++ fron-end/src/_nav.js
@@ -328,11 +328,11 @@ export default {
           url: '/main/user/profile',
           icon: 'cui-clipboard',
         },
-        // {
-        //   name: 'Avatar',
-        //   url: '/main/user/avatar',
-        //   icon: 'cui-camera',
-        // },
+        {
+          name: 'Avatar',
+          url: '/main/user/avatar',
+          icon: 'cui-camera',
+        },
         // {
         //   name: 'Notification',
         //   url: '/main/user/notification',
--- fron-end/src/views/User/Avatar/Avatar.js
+++ fron-end/src/views/User/Avatar/Avatar.js
@@ -1,39 +1,80 @@
-// import React, { Component } from 'react';
-// import '../../../css/profile_card.css';
-// import Dropzone from 'react-dropzone';
-// import request from 'superagent';
-// import { Col, Card } from 'reactstrap';
-//
-// const CLOUDINARY_UPLOAD_PRESET = 'your_upload_preset_id';
-// const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/your_cloudinary_app_name/upload';
-//
-// class ContactForm extends Component {
-//
-//   render() {
-//     return (
-//       <Dropzone
-//         multiple={false}
-//         accept="image/jpg,image/png"
-//         onDrop={this.onImageDrop.bind(this)}>
-//         <p>Drop an image or click to select a file to upload.</p>
-//       </Dropzone>
-//     );
-//   }
-// }
-//
-// class Avatar extends Component {
-//
-//   render() {
-//     return(
-//       <Col xs="24" sm="12" lg="6">
-//         <Card>
-//           <div className="profile-card">
-//             aa
-//           </div>
-//         </Card>
-//       </Col>
-//     );
-//   }
-// }
-//
-// export default Avatar;
+import React, { Component } from 'react';
+import '../../../css/profile_card.css';
+import Dropzone from 'react-dropzone';
+import request from 'superagent';
+import { Col, Card } from 'reactstrap';
+
+const CLOUDINARY_UPLOAD_PRESET = 'mgimss';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/breezeeee/upload';
+
+class ContactForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      uploadedFileCloudinaryUrl: ''
+    };
+  }
+
+  onImageDrop = (files) => {
+    this.setState({
+      uploadedFile: files[0]
+    });
+    this.handleImageUpload(files[0]);
+  };
+
+  handleImageUpload = (file) => {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+      .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+      .field('file', file);
+    upload.end((err, response) => {
+      if (err) {
+        console.error(err);
+      }
+      if (response.body.secure_url !== '') {
+        this.setState({
+          uploadedFileCloudinaryUrl: response.body.secure_url
+        });
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="profile-card">
+        <div className="FileUpload">
+          <Dropzone
+            multiple={false}
+            accept="image/jpeg,image/png"
+            onDrop={this.onImageDrop}>
+            <p>Drop an image or click to select a file to upload.</p>
+          </Dropzone>
+        </div>
+        <div>
+          {this.state.uploadedFileCloudinaryUrl === '' ? null :
+            <div>
+              <p>{this.state.uploadedFile.name}</p>
+              <img src={this.state.uploadedFileCloudinaryUrl} />
+            </div>
+          }
+        </div>
+      </div>
+
+    );
+  }
+}
+
+class Avatar extends Component {
+
+  render() {
+    return(
+      <Col xs="24" sm="12" lg="6">
+        <Card>
+          <ContactForm/>
+        </Card>
+      </Col>
+    );
+  }
+}
+
+export default Avatar;
