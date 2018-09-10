dd54c94c050c293371a56d9197cc228a0a648243
fyc1007261 <fyc1007261@live.com>
2018-09-10 09:59:24 +0800

    update unit tests

--- back-end/src/test/java/com/mgimss/mgimss/controller/OperateApplianceImplTest.java
+++ back-end/src/test/java/com/mgimss/mgimss/controller/OperateApplianceImplTest.java
@@ -116,6 +116,7 @@ public void before() throws Exception {
     operateAppliance.runningJobRepository = mockRJobRepo;
     operateAppliance.finishedJobRepository = mockFNJobRepo;
     operateAppliance.dailyRepository = mockDailyRepo;
+    operateAppliance.userRepository = userRepository;
 
 
 } 
